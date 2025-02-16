61760006051692d617a9b3e21a1910aed35496cc
Tin Hoang Nguyen <tinhoangnguyen111002@gmail.com>
2025-02-16 01:02:58 +0100

    add AuctionDetailView and MainAuctionView

--- src/Bussiness/Bid.java
+++ src/Bussiness/Bid.java
@@ -1,5 +1,14 @@
 package Bussiness;
 
 public class Bid {
-    
+    String seller;
+    int bid;
+    String fileName;
+    boolean won = false;
+
+    public Bid(String seller, int bid, String fileName) {
+        this.seller = seller;
+        this.bid = bid;
+        this.fileName = fileName;
+    }
 }
